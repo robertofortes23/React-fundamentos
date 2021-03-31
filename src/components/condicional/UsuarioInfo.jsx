c457766df841ae9d7dbff568c4f9a718f37602b5
Roberto Fortes <robertbernd23@gmail.com>
2021-03-31 14:38:34 -0300

    Renderizacao condicional

---
+++ src/components/condicional/UsuarioInfo.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import If, {Else} from './If'
+
+export default (props) => {
+    const usuario = props.usuario || {}
+    return (
+        <div>
+            {/* <If test={usuario && usuario.nome}> 
+                Seja Bem Vindo <strong>{usuario.nome}</strong> !
+            </If>
+            
+            <If test={!usuario || !usuario.nome}> 
+                Seja Bem Vindo <strong>Amigão</strong> !
+            </If> */}
+
+            <If test={usuario && usuario.nome}>
+                Seja Bem Vindo <strong>{usuario.nome}</strong>
+                <Else >
+                    Seja Bem <strong>Amigão</strong>
+                </Else>
+            </If>
+
+        </div>
+    )
+}
