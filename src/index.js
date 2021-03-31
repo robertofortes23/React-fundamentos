f00d367ea0c0e3a3b896485e477a56ad60d1aabe
Roberto Fortes <robertbernd23@gmail.com>
2021-03-31 14:33:19 -0300

    Primeiro commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './App'
+
+const el = document.getElementById('root')
+
+ReactDOM.render(
+  <App />
+  ,
+  el
+)
